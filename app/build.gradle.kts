242c9a6ef990f3b67c542fba8be47d968a272686
mystery2323 <malikabubakerwains@gmail.com>
2025-07-25 02:06:14 +0500

    🔥 Fix: Replaced alias(libs...) with direct plugin IDs for Gradle compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
 }
 
